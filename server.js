a9302ab6171c9b304659593f658f0b3c262e7114
Vadim Petrunin <petrunin-vn@yandex.ru>
2018-03-05 21:03:59 +0300

    npm init. Simple server

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+server.listen(5000, function() {
+    console.log('Запускаю сервер на порте 5000');
+});
+
+// Обработчик веб-сокетов
+io.on('connection', function(socket) {
+});
+
+setInterval(function() {
+    io.sockets.emit('message', 'hi!');
+}, 1000);
